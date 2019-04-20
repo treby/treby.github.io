d19ce16f04e12d2269f84764dd40c7562f98ef84
Hiroaki Ninomiya <me@treby.info>
2019-04-21 06:47:23 +0900

    update 20190421.

--- sw.js
+++ sw.js
@@ -2,20 +2,20 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/0b927d57e3a862e455bf.js",
-    "revision": "bbe34b9e49d3b69e110138a01b4af3df"
-  },
-  {
     "url": "/_nuxt/25b0236390c24ed43530.js",
     "revision": "f8c652a05cf2b39978681bfcf10de3f8"
   },
   {
-    "url": "/_nuxt/a6bebe6c713168a02dd5.js",
-    "revision": "4d85e981df58d5047ae605d61c3d502e"
+    "url": "/_nuxt/5f13649f979fc3bba387.js",
+    "revision": "dc9bd941a1b9d352c03813534d925811"
   },
   {
-    "url": "/_nuxt/d7241a23d8b562c0c5ee.js",
-    "revision": "4aa57097302beba5f74d6fc642b130f1"
+    "url": "/_nuxt/84d9cbbf0dfec63031fe.js",
+    "revision": "204c5b0770a1ef1efc9ab2b11eed7311"
+  },
+  {
+    "url": "/_nuxt/a6bebe6c713168a02dd5.js",
+    "revision": "4d85e981df58d5047ae605d61c3d502e"
   },
   {
     "url": "/_nuxt/e2e9bbfab7d5d8b6e7e7.js",
